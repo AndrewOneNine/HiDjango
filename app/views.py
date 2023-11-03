92fef9ac6dfcd4474e9866228aee44a0d63d1d34
Andrey S <archer.archi@bk.ru>
2023-11-03 12:11:46 +0300

    First Django

---
+++ app/views.py
@@ -0,0 +1,36 @@
+import datetime
+import json
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+from first_project.settings import BASE_DIR
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    list_workdir = os.listdir(BASE_DIR)
+    your_list_as_json = json.dumps(list_workdir)
+    return HttpResponse(your_list_as_json)
